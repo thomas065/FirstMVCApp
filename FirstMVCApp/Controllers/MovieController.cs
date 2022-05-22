2a35e07c1238427a30fb606fcd769dd78d6d2bf3
Thomas Bell <thomasjbell065@gmail.com>
2022-05-22 19:42:12 -0400

    Initial commit

---
+++ FirstMVCApp/Controllers/MovieController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
